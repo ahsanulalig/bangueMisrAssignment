05a21fff45504a88c5febb42b42484aa1b9b100e
Ahsanul Haque <ahsanulalig@gmail.com>
2021-03-18 14:11:36 +0530

    budget component implemented

--- src/app/component/budget/budget.component.ts
+++ src/app/component/budget/budget.component.ts
@@ -7,29 +7,44 @@ import { AppService } from 'src/app/service/app.service';
   styleUrls: ['./budget.component.scss'],
 })
 export class BudgetComponent implements OnInit {
-  tiles: Tile[] = [
-    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
-    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
-    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
-    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
+  displayedColumns: string[] = [
+    'name',
+    'last_month',
+    'first_month',
+    'last_modified_on',
   ];
+  budgetsData;
+  budgets;
+  singleBudget;
+  selectedRowIndex;
   constructor(private appService: AppService) {}
-
   ngOnInit(): void {
     this.appService
       .get('https://api.youneedabudget.com/v1/budgets?include_accounts=true')
       .subscribe(
         (resp) => {
-          console.log(' Data List ', resp);
+          this.budgetsData = resp;
+          this.budgets = this.budgetsData.data.budgets;
+          this.singleBudget = this.budgets[0];
+          this.appService.selectedBudgetId = this.singleBudget.id;
         },
         (error) => {}
       );
   }
-}
-
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
+  showBudgetDetail(selectedData, index) {
+    this.selectedRowIndex = index;
+    if (selectedData.id) {
+      this.appService.selectedBudgetId = selectedData.id;
+      let url = `https://api.youneedabudget.com/v1/budgets/${this.appService.selectedBudgetId}`;
+      this.appService.get(url).subscribe(
+        (resp) => {
+          console.log('Response Selected budget ', resp);
+          this.singleBudget = resp;
+        },
+        (err) => {
+          console.log('Error ', err);
+        }
+      );
+    }
+  }
 }
