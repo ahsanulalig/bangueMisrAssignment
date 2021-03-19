e0060896a47f5342a07fea6f6da216e18c07a6bb
Lohit Mishra <lohit.mishra@xebia.com>
2021-03-19 22:36:59 +0530

    comment removes

--- src/app/service/token-interceptor.service.ts
+++ src/app/service/token-interceptor.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import {
   HttpInterceptor,
   HttpRequest,
   HttpHandler,
   HttpEvent,
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { AuthService } from './auth.service';
+} from "@angular/common/http";
+import { Observable } from "rxjs";
+import { AuthService } from "./auth.service";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class TokenInterceptorService implements HttpInterceptor {
   constructor(private authService: AuthService) {}
@@ -17,18 +17,13 @@ export class TokenInterceptorService implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    // All HTTP requests are going to go through this method
     req = req.clone({
       setHeaders: {
-        'Content-Type': 'application/json; charset=utf-8',
-        Accept: 'application/json',
+        "Content-Type": "application/json; charset=utf-8",
+        Accept: "application/json",
         Authorization: `Bearer ${this.authService.token}`,
       },
     });
-    // if (req.body) {
-    //   const duplicate = req.clone({ body: req.body.replace(/pizza/gi, '🍕') });
-    //   return next.handle(duplicate);
-    // }
     return next.handle(req);
   }
 }
