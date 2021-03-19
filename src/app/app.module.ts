b74ac2a5629ad20ada65ba61decc98c1162a0084
Ahsanul Haque <ahsanulalig@gmail.com>
2021-03-19 21:50:50 +0530

    mat component separated from app moudle to different module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,17 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatButtonModule } from "@angular/material/button";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { MatTableModule } from "@angular/material/table";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import { OnBoardingComponent } from "./component/onBoarding/onBoarding.component";
-import { TokenInterceptorService } from "./service/token-interceptor.service";
-import { CreateAccountComponent } from "./component/create-account/create-account.component";
-import { PayeeComponent } from "./component/payee/payee.component";
-import { FormsModule } from "@angular/forms";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { AddAccountModalComponent } from "./modals/add-account-modal/add-account-modal.component";
-import { MatSelectModule } from "@angular/material/select";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { OnBoardingComponent } from './component/onBoarding/onBoarding.component';
+import { TokenInterceptorService } from './service/token-interceptor.service';
+import { CreateAccountComponent } from './component/create-account/create-account.component';
+import { PayeeComponent } from './component/payee/payee.component';
+import { FormsModule } from '@angular/forms';
+import { AddAccountModalComponent } from './modals/add-account-modal/add-account-modal.component';
+import { MatModuleModule } from './module/mat-module/mat-module.module';
 
 @NgModule({
   declarations: [
@@ -35,16 +27,8 @@ import { MatSelectModule } from "@angular/material/select";
     BrowserAnimationsModule,
     CommonModule,
     HttpClientModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatGridListModule,
-    MatTableModule,
-    MatProgressSpinnerModule,
     FormsModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
+    MatModuleModule,
   ],
   providers: [
     {
--- src/app/service/token-interceptor.service.ts
+++ src/app/service/token-interceptor.service.ts
@@ -1,29 +1,28 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import {
   HttpInterceptor,
   HttpRequest,
   HttpHandler,
   HttpEvent,
-} from "@angular/common/http";
-import { Observable } from "rxjs";
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class TokenInterceptorService implements HttpInterceptor {
-  constructor() {}
+  constructor(private authService: AuthService) {}
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     // All HTTP requests are going to go through this method
-    const authToken =
-      "3bf7f00401220e2251c92a881288c458f29541ed60547d1aa8529d7f9badb793";
     req = req.clone({
       setHeaders: {
-        "Content-Type": "application/json; charset=utf-8",
-        Accept: "application/json",
-        Authorization: `Bearer ${authToken}`,
+        'Content-Type': 'application/json; charset=utf-8',
+        Accept: 'application/json',
+        Authorization: `Bearer ${this.authService.token}`,
       },
     });
     // if (req.body) {
